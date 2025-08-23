6ae6d7ba8cc038f4a0c4dd6ab158b46b15533347
DailyLollipops <clarencemadrigal84@gmail.com>
2025-08-23 20:26:10 +0800

    Fix dynamic schema generator

--- api/routes/utils/crudutils.py
+++ api/routes/utils/crudutils.py
@@ -20,14 +20,14 @@ def make_crud_schemas[T: SQLModel](
     Generate Pydantic CRUD schemas dynamically from a SQLModel class.
     Returns: (Create, Update, Response, ListResponse)
     """
-    if addtl_excluded_update_fields is None:
-        addtl_excluded_update_fields = []
-    if addtl_excluded_create_fields is None:
-        addtl_excluded_create_fields = []
-    def get_fields(exclude: list[str]) -> dict:
+    excluded_create_fields = ['modified_by_id', 'created_at', 'updated_at'] + (addtl_excluded_create_fields or [])
+    excluded_update_fields = ['created_at', 'updated_at'] + (addtl_excluded_update_fields or [])
+    excluded_response_fields = ['password', 'api']
+
+    def get_create_fields() -> dict:
         fields = {}
         for name, field in model_cls.model_fields.items():
-            if name in exclude:
+            if name in excluded_create_fields:
                 continue
             if primary_key := getattr(field, 'primary_key', None):
                 if primary_key is True:
@@ -35,24 +35,36 @@ def make_crud_schemas[T: SQLModel](
             fields[name] = (field.annotation, field.default if field.default is not None else ...)
         return fields
 
-    fields = model_cls.model_fields
-    excluded_create_fields = ['modified_by_id', 'created_at', 'updated_at'] + (addtl_excluded_create_fields or [])
-    excluded_update_fields = ['created_at', 'updated_at'] + (addtl_excluded_update_fields or [])
-    excluded_response_fields = ['password', 'api']
+    def get_update_fields() -> dict:
+        fields = {}
+        for name, field in model_cls.model_fields.items():
+            if name in excluded_update_fields:
+                continue
+            if primary_key := getattr(field, 'primary_key', None):
+                if primary_key is True:
+                    continue
+            fields[name] = (field.annotation, None)
+        return fields
 
-    create_fields = get_fields(excluded_create_fields)
+    def get_response_fields() -> dict:
+        fields = {}
+        for name, field in model_cls.model_fields.items():
+            if name in excluded_response_fields:
+                continue
+            if hasattr(field, 'sa_relationship'):
+                continue
+            if name.startswith('_'):
+                continue
+            fields[name] = (field.annotation, ...)
+        return fields
+
+    create_fields = get_create_fields()
     CreateSchema = create_model(f'{model_cls.__name__}Create', **create_fields) # type: ignore
 
-    update_fields = get_fields(excluded_update_fields)
+    update_fields = get_update_fields()
     UpdateSchema = create_model(f'{model_cls.__name__}Update', **update_fields) # type: ignore
 
-    response_fields = {
-        name: (field.annotation, ...)
-        for name, field in fields.items()
-        if not name.startswith('_') \
-            and not hasattr(field, 'sa_relationship') \
-            and name not in excluded_response_fields
-    }
+    response_fields = get_response_fields()
     ResponseSchema = create_model(f'{model_cls.__name__}Response', **response_fields) # type: ignore
 
     ListResponseSchema = create_model(
