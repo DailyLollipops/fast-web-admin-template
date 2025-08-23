994b09f8200d0adfec0f0096a89979b2522c1c3c
DailyLollipops <clarencemadrigal84@gmail.com>
2025-08-23 22:27:13 +0800

    Hide application settings if not system account

--- web/src/layout/default/Menu.tsx
+++ web/src/layout/default/Menu.tsx
@@ -1,20 +1,25 @@
-import { Menu as RaMenu } from "react-admin";
+import { Menu as RaMenu, useGetIdentity } from "react-admin";
 import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import LanguageIcon from "@mui/icons-material/Language";
 
-export const Menu = () => (
-  <RaMenu>
-    <RaMenu.DashboardItem />
-    <RaMenu.ResourceItems />
-    <RaMenu.Item
-      to="/settings"
-      primaryText="App Settings"
-      leftIcon={<AdminPanelSettingsIcon />}
-    />
-    <RaMenu.Item
-      to="/about"
-      primaryText="About App"
-      leftIcon={<LanguageIcon />}
-    />
-  </RaMenu>
-);
+export const Menu = () => {
+  const { identity } = useGetIdentity();
+  return (
+    <RaMenu>
+      <RaMenu.DashboardItem />
+      <RaMenu.ResourceItems />
+      {identity?.role === "system" && (
+        <RaMenu.Item
+          to="/settings"
+          primaryText="App Settings"
+          leftIcon={<AdminPanelSettingsIcon />}
+        />
+      )}
+      <RaMenu.Item
+        to="/about"
+        primaryText="About App"
+        leftIcon={<LanguageIcon />}
+      />
+    </RaMenu>
+  );
+};
